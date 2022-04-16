b1dd536152906cc3d56da86355ebc1c2eaffffa7
Josua <aguilar.josua13011069@gmail.com>
2022-04-16 12:53:20 -0500

    Se agregan filtros de busqueda agrupados en una grilla de 2 x 2

--- RazorPagesMovies/wwwroot/js/site.js
+++ RazorPagesMovies/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const UPriceText = price => {
+    $("#MoviePriceLabel").text("$" + price);
+}
+const UReleaseDate = () => {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1; //La funcion obtiene el indice del mes, por eso se agreaga 1.
+    var year = date.getFullYear();
+    //Parseo dia y mes para cumplir el formato yyyy/mm/dd
+    day = day < 9 ? "0" + day : day;
+    month = month < 9 ? "0" + month : month;
+    $("#MovieReleaseDate").val(year + "-" + month + "-" + day);
+}
+UReleaseDate();
+UPriceText($("#MoviePriceRange").val());
+
+$("#MoviePriceRange").change(function () {
+    UPriceText($("#MoviePriceRange").val());
+});
